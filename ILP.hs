bc9d34824efd6d2b4d7170227496ab79e85431d6
Tristan Konolige <tristan.konolige@gmail.com>
2015-02-15 21:35:40 -0800

    tried State monad, fails inside logict

--- ILP.hs
+++ ILP.hs
@@ -3,14 +3,16 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RankNTypes #-}
 
-import ClassyPrelude
+import ClassyPrelude hiding (union)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Data.Foldable hiding (sequence_, forM_)
 
 -- for equivalence relations
-import Data.Equivalence.Monad
-import Control.Monad.ST.Trans
+-- import Data.Equivalence.Monad
+-- import Control.Monad.ST.Trans
+import UnionFind
+import Control.Monad.State.Lazy (State, evalState, get, put)
 
 import Control.Monad.Logic hiding (sequence_, forM_, mapM, forM)
 
@@ -40,8 +42,7 @@ type Database = Map Symbol [Clause]
 
 -- | A backtracking equivalence monad
 -- The equivalence is on the inside so that backtracking can change it
---                  v-- this 's' makes sure two BacktrackEquiv monads never mix
-type BacktrackEquiv s = LogicT (EquivM s Rep Variable)
+type BacktrackEquiv = LogicT (UnionFind Variable)
 
 clause1 = Clause "happy" [Var "X"] (Check "eats" [Var "X"])
 clause4 = Clause "happy" [Var "X"] (Unify (Var "X") (Atom "phil"))
@@ -53,6 +54,7 @@ database = Map.fromList [("happy", [clause1, clause4]), ("eats", [clause2, claus
 addToDatabase :: Clause -> Database -> Database
 addToDatabase clause@(Clause sym _ _) = insertWith (++) sym [clause]
 
+{-
 -- | union two variable representations
 unionRep a b = case (a, b) of
                  (Placeholder a, Placeholder b) -> Placeholder $ Set.union a b
@@ -62,34 +64,41 @@ unionRep a b = case (a, b) of
 -- | create a Rep from a Variable
 toRep v@(Var _ ) = Placeholder $ Set.singleton v
 toRep a@(Atom _) = Concrete a
+-}
 
 -- | lookup a symbol in the relation database
 -- will return a list of matching clauses
-lookupSymbol :: Database -> Symbol -> BacktrackEquiv s [Clause]
+lookupSymbol :: Database -> Symbol -> BacktrackEquiv [Clause]
 lookupSymbol d x = case lookup x d of
                      Just a  -> return a
                      Nothing -> mzero
 
 -- | unify two values, must be lifted into the outermost monad
 -- TODO: forbit unioning two values
-unify :: Variable -> Variable -> BacktrackEquiv s ()
+unify :: Variable -> Variable -> BacktrackEquiv ()
 unify a b = do
-  descA <- lift $ classDesc a
-  descB <- lift $ classDesc b
+  descA <- lift $ repr a
+  descB <- lift $ repr b
   case (descA, descB) of
-    (Concrete c1, Concrete c2) | c1 /= c2 -> do
-      traceShowM (a, b, c1, c2)
-      mzero -- fail, can't union two concrete variables
-    _ -> lift $ equate a b
+    (Atom a1, Atom a2) | a1 /= a2 -> do
+      mzero -- fail, can't union two Atoms variables
+    (Atom _, Atom _) -> return () -- we have the same class
+    (Atom a, Var v) -> lift $ equate (Var v) (Atom a) -- second arguement is the representative
+    (Var v, Atom a) -> lift $ equate (Var v) (Atom a)
+    (Var v1, Var v2) -> lift $ equate (Var v1) (Var v2)
 
+{-
 -- | A helper to run the equivalence monad with our representation
 run :: (forall s. EquivM s Rep Variable a) -> a
 run = runEquivM toRep unionRep
+-}
 
-interpret :: Database -> Body -> BacktrackEquiv s ()
-interpret database (And b1 b2)          = (interpret database b1) >>- (const $ interpret database b2)
-interpret database (Or b1 b2)           = interpret database b1 `interleave` interpret database b2
-interpret database (Check sym vars)     = do
+-- | interpret an ilp mini-prolog program
+-- a state monad cannot be used because logict backtracks incorrectly with State
+interpret :: Equivalence Variable -> Database -> Body -> BacktrackEquiv ()
+interpret equiv database (And b1 b2)          = (interpret equiv database b1) >>- (const $ interpret equiv database b2)
+interpret equiv database (Or b1 b2)           = interpret equiv database b1 `interleave` interpret equiv database b2
+interpret equiv database (Check sym vars)     = do
   clauses <- lookupSymbol database sym
   foldr1 interleave $ -- try each clause independently TODO: foldr or foldl
     (flip map) clauses -- TODO: 'flip map' should really be 'for', but it doesn't work
@@ -100,31 +109,42 @@ interpret database (Check sym vars)     = do
         -- check the body
         interpret database body
       )
-interpret database (Unify var1 var2)    = unify var1 var2
-interpret database LTrue                = return ()
-interpret database LFalse               = mzero
-interpret database (Extend fact clause) = interpret (addToDatabase fact database) clause
+interpret equiv database (Unify var1 var2)    = unify var1 var2
+interpret equiv database LTrue                = return ()
+interpret equiv database LFalse               = mzero
+interpret equiv database (Extend fact clause) = interpret equiv (addToDatabase fact database) clause
 
-solve :: Body -> Database -> [(Variable, Rep)]
-solve query@(Check _ variables) database = run $ do
+solve :: Body -> Database -> [(Variable, Variable)]
+solve query@(Check _ variables) database = (flip evalState) Map.empty $ do
   observeT $ do
     interpret database query
-    mapM (\x -> lift $ classDesc x >>= return . (,) x) variables
+    mapM (\x -> lift $ repr x >>= return . (,) x) variables
 
-solveAll :: Body -> Database -> [[(Variable, Rep)]]
-solveAll query@(Check _ variables) database = run $ do
+solveAll :: Body -> Database -> [[(Variable, Variable)]]
+solveAll query@(Check _ variables) database = (flip evalState) Map.empty $ do
   observeAllT $ do
     interpret database query
-    mapM (\x -> lift $ classDesc x >>= return . (,) x) variables
+    mapM (\x -> lift $ repr x >>= return . (,) x) variables
 
-test :: BacktrackEquiv s Bool
+test :: BacktrackEquiv Bool
 test = m1 `interleave` m2
   where
     m1 = do
       unify (Var "X") (Atom "a")
       mzero
     m2 = do
+      lift $ get >>= traceShowM
       unify (Var "X") (Atom "b")
       return True
+
+test2 :: LogicT (State Bool) Bool
+test2 = m1 `interleave` m2
+  where
+    m1 = do
+      lift $ put True
+      mzero
+    m2 = do
+      s <- get
+      return s
 
 main = return ()
---
+++ equivalence.hs
@@ -0,0 +1,28 @@
+-- A Very slow union find implementation
+module UnionFind where
+
+import Data.Map as M
+import Control.Monad.State.Lazy
+
+repr' :: Ord a => a -> Map a a -> Maybe a
+repr' r d = case M.lookup r d of
+             Just a | a == r -> Just r
+             Just a          -> repr' a d
+             Nothing         -> Nothing -- shouldn't happen
+
+type UnionFind a = State (Map a a)
+
+repr :: Ord a => a -> UnionFind a a
+repr x = do
+  d <- get
+  case repr' x d of
+    Just a  -> return a
+    Nothing -> modify (insert x x) >> return x
+
+-- | equate unions the equivalence classes of two elements
+-- the representation is taken from the second
+equate :: Ord a => a -> a -> UnionFind a ()
+equate a b = do
+  ra <- repr a
+  rb <- repr b
+  modify (adjust (const rb) ra)
